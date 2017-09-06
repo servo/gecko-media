4034e4cc471bb2ba1158c3432e7ec6ae09c91fcc
Philippe Normand <philn@igalia.com>
2017-09-06 11:13:50 +0100

    Format build.rs with rustfmt nightly

--- build.rs
+++ build.rs
@@ -2,65 +2,76 @@ extern crate gcc;
 use std::env;
 
 fn make_builder() -> gcc::Build {
-	let mut b = gcc::Build::new();
-	b.cpp(true);
+    let mut b = gcc::Build::new();
+    b.cpp(true);
 
-	#[cfg(target_os = "macos")]
-	b.flag("-std=c++11");
+    #[cfg(target_os = "macos")]
+    b.flag("-std=c++11");
 
-	b.include("gecko/glue/include/stl_wrappers");
-	b.include("gecko/include");
-	b.include("gecko/glue/include");
+    b.include("gecko/glue/include/stl_wrappers");
+    b.include("gecko/include");
+    b.include("gecko/glue/include");
 
-	#[cfg(target_os = "macos")]
-	env::set_var("CXXFLAGS", "--include gecko/glue/include/mozilla-config-x86_64-apple-darwin.h");
+    #[cfg(target_os = "macos")]
+    env::set_var(
+        "CXXFLAGS",
+        "--include gecko/glue/include/mozilla-config-x86_64-apple-darwin.h",
+    );
 
-	#[cfg(target_os = "linux")]
-	env::set_var("CXXFLAGS", "--include gecko/glue/include/mozilla-config-x86_64-apple-darwin.h");
+    #[cfg(target_os = "linux")]
+    env::set_var(
+        "CXXFLAGS",
+        "--include gecko/glue/include/mozilla-config-x86_64-apple-darwin.h",
+    );
 
-	b.flag("-fno-exceptions");
-	b.define("GECKO_MEDIA_CRATE", "1");
+    b.flag("-fno-exceptions");
+    b.define("GECKO_MEDIA_CRATE", "1");
 
-	b
+    b
 }
 
 fn compile_tests() {
-	let mut b = make_builder();
+    let mut b = make_builder();
 
-	let src_files = [
-		"test.cpp",
-	];
+    let src_files = ["test.cpp"];
 
-	for file_path in src_files.iter().map(|&p| "gecko/test/".to_owned() + p.clone()) {
-		b.file(file_path);
-	}
-	b.compile("geckotest");
+    for file_path in src_files
+        .iter()
+        .map(|&p| "gecko/test/".to_owned() + p.clone())
+    {
+        b.file(file_path);
+    }
+    b.compile("geckotest");
 }
 
 fn compile_gecko_media() {
-	let mut b = make_builder();
+    let mut b = make_builder();
 
-	let src_files = [
-		"dom/media/MediaInfo.cpp",
-		"mfbt/Assertions.cpp",
-		"mfbt/Unused.cpp",
-		"xpcom/ds/nsTArray.cpp",
-	];
-	for file_path in src_files.iter().map(|&p| "gecko/src/".to_owned() + p.clone()) {
-		b.file(file_path);
-	}
+    let src_files = [
+        "dom/media/MediaInfo.cpp",
+        "mfbt/Assertions.cpp",
+        "mfbt/Unused.cpp",
+        "xpcom/ds/nsTArray.cpp",
+    ];
+    for file_path in src_files
+        .iter()
+        .map(|&p| "gecko/src/".to_owned() + p.clone())
+    {
+        b.file(file_path);
+    }
 
-	let glue_files = [
-		"nsString.cpp",
-	];
-	for file_path in glue_files.iter().map(|&p| "gecko/glue/".to_owned() + p.clone()) {
-		b.file(file_path);
-	}
+    let glue_files = ["nsString.cpp"];
+    for file_path in glue_files
+        .iter()
+        .map(|&p| "gecko/glue/".to_owned() + p.clone())
+    {
+        b.file(file_path);
+    }
 
-	b.compile("gecko_media");
+    b.compile("gecko_media");
 }
 
 fn main() {
-	compile_gecko_media();
-	compile_tests();
+    compile_gecko_media();
+    compile_tests();
 }
