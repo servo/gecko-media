6656ca672dc32bec8aee4d074a07957c71bb5b0e
Philippe Normand <philn@igalia.com>
2017-10-05 12:48:33 +0200

    Add servo-tidy file and fix lint issues

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #[allow(unused_extern_crates)]
 
 #[cfg(feature = "pulseaudio")]
@@ -5,7 +9,6 @@ extern crate cubeb_pulse;
 
 #[cfg(test)]
 mod tests {
-
     extern "C" {
         fn TestGecko();
     }
